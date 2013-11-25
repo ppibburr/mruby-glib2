165ca0b8f8bcc1ac695781f4649b5c1b054d626f
ppibburr <tulnor33@gmail.com>
2013-11-24 22:59:35 -0500

    ...

--- samples/main.rb
+++ samples/main.rb
@@ -1,25 +1,36 @@
-path = "./glib_file_test.txt"
-GirFFI::DEBUG[:VERBOSE] = false
+GirFFI::DEBUG[:VERBOSE] = 0==1
+
+def at_exit!(loop)
+  puts "Since this Timeout was called only once: @ii is 1; #{@ii == 1}"
+  puts "The test file contents should be: 123456; #{GLib::File.get_contents(PATH) == "123456"}"
+  
+  loop.quit()
+end
+
+PATH = "./glib_file_test.txt"
 loop = GLib::MainLoop.new(nil,false)
 
 i = 0
 GLib::Timeout.add 200,500 do
-  p i += 1
+  i += 1
   
   if i == 1
     contents = i.to_s
   else
-    contents = GLib::File.get_contents(path)+"#{i}"
+    contents = GLib::File.get_contents(PATH)+"#{i}"
   end
   
-  GLib::File.set_contents(path, contents)
-  loop.quit() if i > 5
-  true
+  GLib::File.set_contents(PATH, contents)
+  
+  at_exit!(loop) if i > 5
+  
+  next true
 end
 
-ii = 0
-GLib::Timeout.add -100,500 do
-  ii += 1
+@ii = 0
+GLib::Timeout.add(-100,500) do
+  @ii += 1
+  
   next false
 end
 
