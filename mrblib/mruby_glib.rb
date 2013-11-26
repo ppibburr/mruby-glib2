a6a02d2fa4d45689181dc85b37f61a86abcb2368
ppibburr <tulnor33@gmail.com>
2013-11-25 21:45:31 -0500

    implement GLib::Spawn (g_spawn_sync and friends)

--- mrblib/mruby_glib.rb
+++ mrblib/mruby_glib.rb
@@ -4,18 +4,20 @@ glib = {
   # :version   => 2.0,
   
   :define => {
-    # :methods => {
-    #   :m_name => {
-    #     :symbol => ... ,
-    #     :alias  => ... ,
-    #     
-    #     :argument_types => [],
-    #     :return_type    => ...
-    #   }
-    # },  
-    
     :classes => {
       :Timeout => {
+        :class_methods => {
+          :add => {
+            :symbol => :timeout_add
+          },
+          
+          :add_seconds => {
+            :symbol => :timeout_add_seconds
+          }
+        }
+      },
+      
+      :Idle => {
         :class_methods => {
           :add => {
             :symbol => :timeout_add
@@ -37,9 +39,145 @@ glib = {
             :symbol => :file_set_contents
           }          
         }
+      },
+      
+      :Spawn => {
+        :class_methods => {
+          :sync => {
+            :symbol => :spawn_sync
+          },
+          
+          :async => {
+            :symbol => :spawn_async
+          },
+          
+          :command_line_sync => {
+            :symbol => :spawn_command_line_sync
+          },
+          
+          :command_line_async => {
+            :symbol => :spawn_command_line_async
+          }
+        }
       }
     }
   }
 }
 
 GirFFI.describe glib
+
+GLib::SpawnFlags
+GLib::Lib.attach_function :g_spawn_command_line_sync, [:string,:pointer,:pointer,:pointer,:pointer], :bool
+GLib::Lib.attach_function :g_spawn_command_line_async, [:string,:pointer], :bool
+GLib::Lib.attach_function :g_spawn_async, [:string, :pointer, :pointer, :SpawnFlags, :pointer, :pointer, :pointer, :pointer], :bool
+GLib::Lib.attach_function :g_spawn_sync, [:string, :pointer, :pointer, :SpawnFlags, :pointer, :pointer, :pointer, :pointer, :pointer, :pointer], :bool
+GLib::Lib.attach_function :g_shell_parse_argv, [:string, :pointer, :pointer, :pointer], :bool
+def GLib.shell_parse_argv str
+  out = FFI::MemoryPointer.new(:pointer)
+  
+  self::Lib.g_shell_parse_argv(str, nil.to_ptr, out, nil.to_ptr)
+  
+  return out
+end
+
+def GLib.spawn_command_line_async cmd
+  error = FFI::MemoryPointer.new(:pointer)
+  error.write_pointer FFI::Pointer::NULL
+
+  bool = self::Lib.g_spawn_command_line_async(cmd, error.to_out(true))
+  
+  unless bool
+    raise GLib::Error.new(error).message
+  end
+  
+  return bool
+end
+
+def GLib.spawn_command_line_sync cmd
+  error = FFI::MemoryPointer.new(:pointer)
+  error.write_pointer FFI::Pointer::NULL
+
+  status    = FFI::MemoryPointer.new(:pointer)
+  out       = FFI::MemoryPointer.new(:pointer)
+  err       = FFI::MemoryPointer.new(:pointer)
+
+  bool = self::Lib.g_spawn_command_line_sync(cmd, out, err, status, error.to_out(true))
+  
+  unless bool
+    raise GLib::Error.new(error).message
+  end
+  
+  e = err.get_pointer(0).to_s
+  o = out.get_pointer(0).to_s
+  
+  return status.get_pointer(0).read_int, o, e
+end
+
+def GLib.spawn_helper(*argv,&b)
+  argvp = GLib::shell_parse_argv(argv.join(" "))
+  
+  cb_info = GirFFI::REPO.find_by_name("GLib", "SpawnChildSetupFunc")
+  cb_info.extend GirFFI::Builder::MethodBuilder::Callable
+  
+  cb = cb_info.make_closure(&b)
+  
+  error = FFI::MemoryPointer.new(:pointer)
+  error.write_pointer FFI::Pointer::NULL
+  
+  return argvp,cb,error
+end
+
+# Exectues a process asyncrously
+#
+# @param path String, the path to execute in
+# @param argv Array<String>, the argument vector
+# @param flags Integer, represents the GLib::SpawnFlags
+# @param b Proc, the block to call before execution
+#
+# @return Integer, Child PID
+def GLib.spawn_async(path, argv, flags=0,&b)
+
+  argvp, cb, error = spawn_helper(*argv,&b)
+
+  status = FFI::MemoryPointer.new(:pointer)
+  
+  out    = FFI::MemoryPointer.new(:pointer)
+  err    = FFI::MemoryPointer.new(:pointer)
+  
+  bool = self::Lib.g_spawn_sync path, argvp.get_pointer(0), nil.to_ptr, flags, cb, nil.to_ptr, pid, error.to_out(true)
+  
+  unless bool
+    raise GLib::Error.new(error).message
+  end
+  
+  return pid.get_pointer(0).read_int
+end
+
+# Exectues a process syncrously
+#
+# @param path String, the path to execute in
+# @param argv Array<String>, the argument vector
+# @param flags Integer, represents the GLib::SpawnFlags
+# @param b Proc, the block to call before execution
+#
+# @return Array of Integer (exit code), String (stdout), String (stderr)
+def GLib.spawn_sync(path, argv, flags=0,&b)
+
+  argvp, cb, error = spawn_helper(*argv,&b)
+
+  status = FFI::MemoryPointer.new(:pointer)
+  
+  out    = FFI::MemoryPointer.new(:pointer)
+  err    = FFI::MemoryPointer.new(:pointer)
+  
+  bool = self::Lib.g_spawn_sync path, argvp.get_pointer(0), nil.to_ptr, flags, cb, nil.to_ptr, out, err, status, error.to_out(true)
+  
+  unless bool
+    raise GLib::Error.new(error).message
+  end
+  
+  e = err.get_pointer(0).to_s
+  o = out.get_pointer(0).to_s
+  
+  return status.get_pointer(0).read_int, o, e
+end
