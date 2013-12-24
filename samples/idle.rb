a3e8aa88d011779e271dd69ba97262fcabf560aa
ppibburr <tulnor33@gmail.com>
2013-12-23 19:28:27 -0500

    ...

--- samples/idle.rb
+++ samples/idle.rb
@@ -1,7 +1,18 @@
-GLib::Idle::add -1 do
+loop = GLib::MainLoop.new(false)
+
+cnt = 0
+GLib::Idle::add 200 do
   puts "idle"
+  
+  (cnt += 1) != 1000
+end
+
+GLib::Idle::add 200 do
+  if cnt >= 1000
+    loop.quit()
+  end
+  
   true
 end
 
-loop = GLib::MainLoop.new(false)
 loop.run
